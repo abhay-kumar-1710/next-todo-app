a16fde056d1c612b8e4b211289cb44aeaed84a0c
Abhi <abhaychinu123@gmail.com>
2025-05-14 21:42:33 +0530

    Initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,15 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -10,10 +20,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={` antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <script src="https://cdn.lordicon.com/lordicon.js"></script>
       </body>
     </html>
   );
