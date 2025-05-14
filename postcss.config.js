0935a210ffa6b647ca38eea0849fec98098f4c5f
Abhi <abhaychinu123@gmail.com>
2025-05-14 19:41:06 +0530

    Initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,10 @@
 
 // export default config;
 
-const config = {
+// postcss.config.js
+module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
-};
-export default config;
+}
