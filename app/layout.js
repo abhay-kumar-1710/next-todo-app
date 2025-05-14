d065c945fa12bc8b75fba710a1177be34d3af135
Abhi <abhaychinu123@gmail.com>
2025-05-14 21:54:07 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-<script src="https://cdn.lordicon.com/lordicon.js"></script>
+
       </body>
     </html>
   );
